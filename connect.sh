c8caecdcb2baca20223f2a8f3efa3cfade97e7ea
drcoccodrillus <dr.coccodrillus@gmail.com>
2024-03-15 12:20:40 +0100

    Add script for autoconnect interfaces

---
+++ connect.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+snap connect yandex-browser:alsa
+snap connect yandex-browser:audio-record
+snap connect yandex-browser:bluez
+snap connect yandex-browser:camera
+snap connect yandex-browser:joystick
+snap connect yandex-browser:mount-observe
+snap connect yandex-browser:mpris
+snap connect yandex-browser:password-manager-service
+snap connect yandex-browser:raw-usb
+snap connect yandex-browser:removable-media
+snap connect yandex-browser:system-packages-doc
+snap connect yandex-browser:u2f-devices
+snap connect yandex-browser:hardware-observe
+snap connect yandex-browser:network-control
+snap connect yandex-browser:network-manager
+snap connect yandex-browser:network-manager-observe
+snap connect yandex-browser:network-observe
+snap connect yandex-browser:network-setup-control
+snap connect yandex-browser:network-setup-observe
+snap connect yandex-browser:process-control
+#snap connect yandex-browser:pulseaudio
+snap connect yandex-browser:system-observe
